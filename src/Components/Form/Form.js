4b19c40de32a34cacef7039c709162c18edd2616
Angelica Harrison <harrison_angelica@yahoo.com>
2018-08-22 15:25:28 -0400

    working connection between back and front with create funct

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import Home from "../Home/Home";
 import "./Form.css";
+import axios from "axios";
 
 class Form extends Component {
   constructor(props) {
@@ -16,18 +17,47 @@ class Form extends Component {
         image: String
       }
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+
+    // handleChange(event) {
+    //   this.setState({ value: event.target.value });
+    // }
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  addVacationData() {
+    var sightsee = document.querySelector("#sightsee").value;
+    var restaurant = document.querySelector("#restaurant").value;
+    var accomodation = document.querySelector("#accomodation").value;
+    var romanticPlace = document.querySelector("#romanticPlace").value;
+    var image = document.querySelector("#image").value;
+    axios({
+      method: "post",
+      url: "http://localhost:3001/api/helium",
+      data: {
+        sightsee: sightsee,
+        restaurant: restaurant,
+        accomodation: accomodation,
+        romanticPlace: romanticPlace,
+        image: image
+      },
+      config: { headers: { "Content-Type": "multipart/form-data" } }
+    })
+      .then(function(response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function(response) {
+        //handle error
+        console.log(response);
+      });
   }
 
-  handleSubmit(event) {
-    alert("A post was submitted: " + this.state.value);
-    event.preventDefault();
-  }
+  // handleSubmit(event) {
+  //   alert("A post was submitted: " + this.state.value);
+  //   event.preventDefault();
+  // }
 
   render() {
     return (
@@ -45,50 +75,34 @@ class Form extends Component {
             <br />
             <label>
               Sightsee:
-              <input
-                type="text"
-                value={this.state.things}
-                onChange={this.handleChange}
-              />
+              <input type="text" id="sightsee" />
             </label>
             <br />
             <label>
               Restaurant:
-              <input
-                type="text"
-                value={this.state.things}
-                onChange={this.handleChange}
-              />
+              <input type="text" id="restaurant" />
             </label>
             <br />
             <label>
               Accomodation:
-              <input
-                type="text"
-                value={this.state.things}
-                onChange={this.handleChange}
-              />
+              <input type="text" id="accomodation" />
             </label>
             <br />
             <label>
               A Romantic Place:
-              <input
-                type="text"
-                value={this.state.things}
-                onChange={this.handleChange}
-              />
+              <input type="text" id="romanticPlace" />
             </label>
             <br />
             <label>
               Image:
-              <input
-                type="text"
-                value={this.state.things}
-                onChange={this.handleChange}
-              />
+              <input type="text" id="image" />
             </label>
             <br />
-            <input type="submit" value="Submit" />
+            <input
+              onClick={this.addVacationData.bind(this)}
+              type="submit"
+              value="Submit"
+            />
           </form>
         </main>
       </div>
