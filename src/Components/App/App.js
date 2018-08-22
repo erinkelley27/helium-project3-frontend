b4bebef8b441268cc16207dbad9c8d99dabcbc5f
“Erin <erinkelley27@gmail.com>
2018-08-22 16:37:38 -0400

    added user-auth state and method binds

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import Home from '../Home/Home';
-import SeeVacations from '../SeeVacations/SeeVacations';
-import SignUpForm from '../SignUpForm/SignUpForm'
-import LogInForm from '../LogInForm/LogInForm'
-import LogOut from '../LogOut/LogOut'
+import React, { Component } from 'react'
+import { Route, Switch, Link } from 'react-router-dom'
+
+import Home from '../Home/Home'
+import SeeVacations from '../SeeVacations/SeeVacations'
+// import SignUpForm from '../SignUpForm/SignUpForm'
+// import LogInForm from '../LogInForm/LogInForm'
+// import LogOut from '../LogOut/LogOut'
 import axios from 'axios'
 
-import Form from '../Form/Form';
+import Form from '../Form/Form'
 // import seedData from "../data/data.json";
-import './App.css';
+import './App.css'
 
 class App extends Component {
-
-
+  constructor () {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      isLoggedIn: false
+    }
+    this.handleLogOut = this.handleLogOut.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+    this.handleLogIn = this.handleLogIn.bind(this)
+    this.handleSignUp = this.handleSignUp.bind(this)
+  }
   render () {
     return (
       <div className='App'>
@@ -30,13 +41,13 @@ class App extends Component {
         <main>
           <Switch>
             <Route
-              exact
-              path='/see-vacations'
-              render={(routerProps) => <SeeVacations {...routerProps}{...this.state} />}
-            />
-            <Route
               path='/home'
               component={Home}
+            />
+            <Route
+              exact
+              path='/see-vacations'
+              render={(routerProps) => <SeeVacations {...routerProps} {...this.state} />}
             />
           </Switch>
         </main>
