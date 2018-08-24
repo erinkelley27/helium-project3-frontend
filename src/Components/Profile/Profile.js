7003142b3bbb4520a5773492dcde05198094d6fe
Angelica Harrison <harrison_angelica@yahoo.com>
2018-08-24 10:39:08 -0400

    commit before revert to previous commit bc40abc

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { Route, Switch, Link } from "react-router-dom";
-import axios from "axios";
-=======
 import axios from 'axios'
->>>>>>> b589ed32474562733bb69d662523bec10a8b9210
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
-      things2do: []
-      // profileData: []
-    };
-=======
     things2do: []
     }
->>>>>>> b589ed32474562733bb69d662523bec10a8b9210
   }
 
   deletethings2do() {
@@ -39,46 +28,17 @@ class Profile extends Component {
       });
   }
 
-<<<<<<< HEAD
-  // componentWillMount() {
-  //   let profile = this.props.cityData.find(
-  //     profile => profile.symbol === this.props.match.params.symbol
-  //   ).then(this.setState({prordata : data}))
-
-  // }
-
-  componentDidMount() {
-    axios
-      .get("http://localhost:3001/api/helium/things2do")
-      .then(response => {
-        this.setState({
-          things2do: response.data
-        });
-=======
   
   componentDidMount(){
     axios.get('http://localhost:3001/api/helium/things2do')
     .then((response)=>{
       this.setState({
         things2do: response.data
->>>>>>> b589ed32474562733bb69d662523bec10a8b9210
       })
       .catch(err => {
         console.log(err);
       });
   }
-<<<<<<< HEAD
-
-  render() {
-    //needs to be in lifecycle event componentWillMount (maybe componentDidMount)
-    //https://stackoverflow.com/questions/30929679/react-fetch-data-in-server-before-render
-    let profile = this.props.cityData.find(
-      profile => profile.symbol === this.props.match.params.symbol
-    );
-
-    let things2do = this.state.things2do.map(item => {
-      return (
-=======
 
   render() {
     let profile = this.props.cityData.find(
@@ -86,7 +46,6 @@ class Profile extends Component {
     )
     let things2do = this.state.things2do.map(item => {  
       return(
->>>>>>> b589ed32474562733bb69d662523bec10a8b9210
         <div>
           <p>{item.sightsee}</p>
           <p>{item.restaurant}</p>
@@ -94,16 +53,9 @@ class Profile extends Component {
           <p>{item.romanticPlace}</p>
           <img src={item.image} />
         </div>
-<<<<<<< HEAD
-      );
-    });
-    console.log("test");
-
-=======
-        
+  
       )
     })
->>>>>>> b589ed32474562733bb69d662523bec10a8b9210
     return (
       <div>
         <p>{profile.city}</p>
@@ -116,8 +68,7 @@ class Profile extends Component {
           onClick={this.deletethings2do.bind(this)}
         />
       </div>
-    );
+    )}
   }
-}
 
 export default Profile;
